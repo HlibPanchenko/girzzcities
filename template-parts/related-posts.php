ec0e4d32dd5a3cfe1bbd74546eb8f187dda14ebf
HlibPanchenko <glebpanchenk7@gmail.com>
2025-07-30 11:11:48 +0300

    Fix: related posts sync with current city

--- template-parts/related-posts.php
+++ template-parts/related-posts.php
@@ -1,8 +1,9 @@
 <?php
-$current_post_id = get_the_ID();
+use ESC\Luna\Modules\CitiesModule;
 
+$current_post_id = get_the_ID();
 $current_hair_color = get_field('model_hair_color', $current_post_id);
-$current_lang = apply_filters( 'wpml_current_language', null ) ?? 'ru';
+$current_lang = apply_filters('wpml_current_language', null) ?? 'ru';
 
 $translations['similar_models'] = [
     'ru' => 'Похожие модели',
@@ -11,18 +12,28 @@ $translations['similar_models'] = [
     'uk' => 'Схожі моделі'
 ];
 
-if (!empty($current_hair_color)) {
+$model_city_terms = wp_get_post_terms($current_post_id, 'city', ['fields' => 'slugs']);
+$current_city_slug = CitiesModule::get_current_city_slug();
+
+if (!empty($current_hair_color) && $current_city_slug) {
     $related_models = new WP_Query([
-        'post_type' => 'models',
-        'post__not_in' => [$current_post_id],
+        'post_type'      => 'models',
+        'post__not_in'   => [$current_post_id],
         'posts_per_page' => 4,
-        'meta_query' => [
+        'meta_query'     => [
             [
-                'key' => 'model_hair_color',
-                'value' => $current_hair_color,
+                'key'     => 'model_hair_color',
+                'value'   => $current_hair_color,
                 'compare' => '=',
             ],
         ],
+        'tax_query' => [
+            [
+                'taxonomy' => 'city',
+                'field'    => 'slug',
+                'terms'    => $current_city_slug,
+            ]
+        ]
     ]);
 
     if ($related_models->have_posts()) {
